d0899dafc8b9fcb6ddbb91cc8b7e4660b8c14515
Alankrit Arya <alankritarya15@gmail.com>
2023-05-04 02:02:29 +0530

    connected data

---
+++ src/screens/Final.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import congrats from './images/congrats.gif'
+
+function Final() {
+  return (
+    <div>
+        <img style={{height:"100vh",width:"100vw"}} src={congrats} alt="congrats"/>
+    </div>
+  )
+}
+
+export default Final
